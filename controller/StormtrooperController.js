010140cf288ac00ce497083e8b76c43199ada25b
João Filho <joao.filho.g3@gmail.com>
2021-01-21 21:49:02 -0300

    Ajustando stormtroopers.js

--- controller/StormtrooperController.js
+++ controller/StormtrooperController.js
@@ -1,28 +1,28 @@
 function StormtrooperController() { }
 
-/** Método controller responsável por chamar a rota que obtém todos os Stormtroopers */
+/** Método controller responsável por obter todos os Stormtroopers */
 StormtrooperController.prototype.getAll = function (request, response, next) {
     response.send("Obter todos os Stormtroopers");
 };
 
-/** Método controller responsável por chamar a rota que obtém um Stormtrooper específico 
+/** Método controller responsável por obter um Stormtrooper específico 
  * de acordo com seu Id */
 StormtrooperController.prototype.getById = function (request, response, next) {
     response.send("Obter Stormtrooper específico");
 };
 
-/** Método controller responsável por chamar a rota que cria um Stormtrooper */
+/** Método controller responsável por criar um Stormtrooper */
 StormtrooperController.prototype.create = function (request, response, next) {
     response.send("Criar novo Stormtrooper");
 };
 
-/** Método controller responsável por chamar a rota que altera um Stormtrooper específico 
+/** Método controller responsável por alterar um Stormtrooper específico 
  * de acordo com seu Id */
 StormtrooperController.prototype.update = function (request, response, next) {
     response.send("Alterando Stormtrooper específico");
 };
 
-/** Método controller responsável por chamar a rota que remove um Stormtrooper específico 
+/** Método controller responsável por remover um Stormtrooper específico 
  * de acordo com seu Id */
 StormtrooperController.prototype.update = function (request, response, next) {
     response.send("Removendo Stormtrooper específico");
--- routes/stormtroopers.js
+++ routes/stormtroopers.js
@@ -1,27 +1,11 @@
 var express = require("express");
 var router = express.Router();
+var StormtrooperController = require("../controller/StormtrooperController")
 
-/** Método responsável por obter todos os Stormtroopers do banco de dados */
-router.get("/", function (request, response) {
-    response.send("Obter todos os stormtroopers");
-});
+router.get("/", StormtrooperController.getAll);
+router.get("/:_id", StormtrooperController.getAll);
+router.post("/", StormtrooperController.getAll);
+router.put("/:_id", StormtrooperController.getAll);
+router.delete("/:_id", StormtrooperController.getAll);
 
-/** Método responsável por obter um Stormtrooper específico de acordo com seu id */
-router.get("/:_id", function (request, response) {
-    response.send("Obter um stormtrooper específico de acordo com seu id");
-});
-
-/** Método responsável por criar um novo Stormtrooper */
-router.post("/", function (request, response) {
-    response.send("Criar novo stormtrooper");
-});
-
-/** Método responsável por alterar um Stormtrooper existente de acordo com seu id */
-router.put("/:_id", function (request, response) {
-    response.send("Alterar um stormtrooper");
-});
-
-/** Método responsável por deletar um Stormtrooper existente de acordo com seu id */
-router.delete("/:_id", function (request, response) {
-    response.send("Deletar um stormtrooper");
-});
+module.exports = router;
